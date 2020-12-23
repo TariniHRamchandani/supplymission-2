3b2cb0fcdc7d04df96b964e2d8798e431df4ccbe
TariniHRamchandani <74418910+TariniHRamchandani@users.noreply.github.com>
2020-12-24 00:19:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,13 @@
+class Box{
+    construtor(){
+        boxpart=createSprite(width/2-100,610,20,100);
+        boxpart.shapeColor="red";
+
+        boxpart2=createSprite(width/2,650,200,20);
+        boxpart2.shapeColor="red";
+
+        boxpart3=createSprite(width/2+100,610,20,100);
+        boxpart3.shapeColor="red";
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var rect1,rect2,rect3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2,450, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+	boxpart=new Box ;
+	boxpart2=new Box;
+	boxpart3=new Box ;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  text(mouseX+":"+mouseY,100,100);
+  drawSprites();
+  boxpart.display();
+  boxpart2.display();
+  boxpart3.display();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	 console.log("hello");
+    Matter.Body.setStatic(packageBody,false);
+
+    
+  }
+}
+
+
+
